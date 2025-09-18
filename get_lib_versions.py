d197229ffab44cf4e5f37a1599d379615866ed86
Rishabh Porwal <rishabhporwal95@gmail.com>
2025-09-19 03:34:54 +0530

    feature: created agentic workflow

--- get_lib_versions.py
+++ get_lib_versions.py
@@ -1,9 +1,23 @@
 import importlib.metadata
 packages = [
+    "beautifulsoup4",
+    "fastapi",
+    "html5l",
+    "jinja2",
     "langchain",
-    "python-dotenv",
+    "langchain-astradb",
     "langchain_core",
-    "streamlit"
+    "langchain-google-genai",
+    "langchain-groq",
+    "lxml",
+    "python-dotenv",
+    "python-multipart",
+    "selenium",
+    "streamlit",
+    "undetected-chromedriver",
+    "uvicorn",
+    "structlog",
+    "langgraph",
 ]
 for pkg in packages:
     try:
